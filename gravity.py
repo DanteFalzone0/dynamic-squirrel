060c21a8f242809120bb9683b11c42e827b6c475
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-03-06 08:00:33 -0600

    Update gravity.py

--- gravity.py
+++ gravity.py
@@ -14,6 +14,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
+"""
+NOTE: For this program to work, you must have the files
+"""
 import pygame
 import os
 import sys
