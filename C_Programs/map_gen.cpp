389d2028d5c73da241184ea1ad6f612ef2303008
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-03-10 23:20:50 -0700

    Update map_gen.cpp
    got rid of trailing spaces

--- C_Programs/map_gen.cpp
+++ C_Programs/map_gen.cpp
@@ -5,27 +5,22 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
- 
-************************************************************************
 
+************************************************************************
 It is recommended that you compile this program in the following manner:
     g++ map_gen.cpp -o mapmaker $(pkg-config --libs --cflags cairo)
 and then run it with the following command:
     ./mapmaker
-
 For that, you'll need to have used the following commands:
     sudo apt install libcairo2-dev
     sudo apt install pkg-config
 ...or whatever is appropriate for you if your distro doesn't use apt.
-
 I will NOT port this to Microsloth Losedoze.
  */
 
@@ -80,13 +75,13 @@ int main(void) {
     draw_green_line(500, 0, 500, 500);
     draw_green_line(500, 500, 0, 500);
     draw_green_line(0, 500, 0, 0);
-    
+
     int side = which_side();
-    
+
     if (side == 0) {
         int xpos = set_pos();
         int ypos = 0;
-        
+
         while (ypos < 500) {
             int new_xpos = xpos + change();
             int new_ypos = ypos + change();
@@ -97,36 +92,36 @@ int main(void) {
     } else if (side == 1) {
         int xpos = 0;
         int ypos = set_pos();
-        
+
         while (xpos < 500) {
             int new_xpos = xpos + change();
             int new_ypos = ypos + change();
             draw_green_line(xpos, ypos, new_xpos, new_ypos);
             xpos = new_xpos;
             ypos = new_ypos;
-        }        
+        }
     } else if (side == 2) {
         int xpos = set_pos();
         int ypos = 500;
-        
+
         while (ypos > 0) {
             int new_xpos = xpos + change();
             int new_ypos = ypos - change();
             draw_green_line(xpos, ypos, new_xpos, new_ypos);
             xpos = new_xpos;
             ypos = new_ypos;
-        }        
+        }
     } else if (side == 3) {
         int xpos = 500;
         int ypos = set_pos();
-        
+
         while (xpos > 0) {
             int new_xpos = xpos - change();
             int new_ypos = ypos + change();
             draw_green_line(xpos, ypos, new_xpos, new_ypos);
             xpos = new_xpos;
             ypos = new_ypos;
-        }        
+        }
     }
 
     cairo_destroy(cr);
