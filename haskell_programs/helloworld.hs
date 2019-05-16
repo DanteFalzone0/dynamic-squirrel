bdb0391062d1ce1d51c5df59d5e40edfa9f39598
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-05-15 21:56:59 -0500

    Create helloworld.hs

---
+++ haskell_programs/helloworld.hs
@@ -0,0 +1,11 @@
+-- This is Dante's first Haskell program. Compile with:
+--  ghc helloworld.hs
+
+-- I'm a bit tired of object-oriented programming.
+
+-- I'm sick of such crufty types as "abstract virtual
+-- private class constructor". (Looking at you, Java.)
+
+-- So I'm going to start coding in a whole new paradigm.
+-- Functional programming FTW!
+main = putStrLn "Hello, world!"
